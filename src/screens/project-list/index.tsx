ddfa67c3742fe8e9e0d3873e2f0d308059a253ca
pxin <1143458272@qq.com>
2021-04-04 01:56:21 +0800

    此版本报错：无法找到模块react的声明文件

--- src/screens/project-list/index.tsx
+++ src/screens/project-list/index.tsx
@@ -1,26 +1,29 @@
+import React from "react";
 import { SearchPanel } from "./search-panel";
 import { List } from "./list";
 import { useEffect, useState } from "react";
-import { cleanObject,useMount,useDebounce } from "../../utils";
+import { cleanObject, useMount, useDebounce } from "../../utils";
 import * as qs from "qs";
-const apiUrl = process.env.REACT_APP_API_URL
+const apiUrl = process.env.REACT_APP_API_URL;
 export const ProjectListScreen = () => {
   const [param, setParam] = useState({
-    name: '',
-    personId: '',
+    name: "",
+    personId: "",
   });
   const [list, setList] = useState([]);
   const [users, setUsers] = useState([]);
-  const debouncedParam = useDebounce(param, 2000)
-  useEffect (() => {
+  const debouncedParam = useDebounce(param, 2000);
+  useEffect(() => {
     //运用qs库可以将下面代码转换为 fetch(`${apiUrl}/projects?name=${param.name}&personId=${param.personId}`)
-    fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(debouncedParam))}`).then(async (response) => {
+    fetch(
+      `${apiUrl}/projects?${qs.stringify(cleanObject(debouncedParam))}`
+    ).then(async (response) => {
       if (response.ok) {
         setList(await response.json());
       }
     });
   }, [debouncedParam]);
-  useMount (() => {
+  useMount(() => {
     fetch(`${apiUrl}/users`).then(async (response) => {
       if (response.ok) {
         setUsers(await response.json());
@@ -33,7 +36,7 @@ export const ProjectListScreen = () => {
   return (
     <div>
       <SearchPanel param={param} setParam={setParam} users={users} />
-      <List list={list} users={users}/>
+      <List list={list} users={users} />
     </div>
   );
 };
--- src/screens/project-list/search-panel.tsx
+++ src/screens/project-list/search-panel.tsx
@@ -1,6 +1,20 @@
 // import { useState } from "react";
-
-export const SearchPanel = ({users, param, setParam}) => {
+export interface User {
+  id: string;
+  name: string;
+  email: string;
+  title: string;
+  organization: string;
+}
+interface SearchPanelProps {
+  users: User[];
+  param: {
+    name: string;
+    personId: string;
+  };
+  setParam: (param: SearchPanelProps["param"]) => void;
+}
+export const SearchPanel = ({ users, param, setParam }: SearchPanelProps) => {
   return (
     <form>
       <div>
@@ -26,7 +40,9 @@ export const SearchPanel = ({users, param, setParam}) => {
         >
           <option value={""}>负责人</option>
           {users.map((user) => (
-            <option key={user.id} value={user.id}>{user.name}</option>
+            <option key={user.id} value={user.id}>
+              {user.name}
+            </option>
           ))}
         </select>
       </div>
