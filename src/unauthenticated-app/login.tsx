77de13ea6493078d318e57ac32313c81b676f47b
pxin <1143458272@qq.com>
2021-04-09 00:59:59 +0800

    bug: 写好antd组件样式后页面没更新

--- src/unauthenticated-app/login.tsx
+++ src/unauthenticated-app/login.tsx
@@ -1,32 +1,33 @@
-import React, { FormEvent } from "react";
+import React from "react";
 import { useAuth } from "../context/auth-context";
+import {Button, Form, Input} from 'antd'
 // const apiUrl = process.env.REACT_APP_API_URL;
 
 export const LoginScreen = ()=> {
   //此login是请求浏览器service-worker的login方式
-  const {login,user} = useAuth()
-  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
+  const {login} = useAuth()
+  const handleSubmit = (values: {username:string,password: string}) => {
+    // event.preventDefault();
     // console.log(event.currentTarget.elements);
-    const username = (event.currentTarget.elements[0] as HTMLInputElement)
-      .value;
-    const password = (event.currentTarget.elements[1] as HTMLInputElement)
-      .value;
-      // console.log(username,password);
-      
-    login({ username, password });
+    // const username = (event.currentTarget.elements[0] as HTMLInputElement)
+    //   .value;
+    // const password = (event.currentTarget.elements[1] as HTMLInputElement)
+    //   .value;      
+    login(values);
   };
   return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label htmlFor="username">用户名</label>
-        <input type="text" id={"username"} />
-      </div>
-      <div>
-        <label htmlFor="password">密码</label>
-        <input type="password" id={"password"} />
-      </div>
-      <button type={"submit"}>登录</button>
-    </form>
+    <Form onFinish={handleSubmit}>
+      <Form.Item name= {'username'} rules={[{required:true, message:'请输入用户名'}]}>
+        {/* <label htmlFor="username">用户名</label> */}
+        <Input placeholder={'用户名'} type="text" id={"username"} />
+      </Form.Item>
+      <Form.Item name= {'password'} rules={[{required:true, message:'请输入密码'}]}>
+        {/* <label htmlFor="password">密码</label> */}
+        <Input placeholder={'密码'} type="password" id={"password"} />
+      </Form.Item>
+      <Form.Item>
+      <Button htmlType={'submit'} type={"primary"}>登录</Button>
+      </Form.Item>
+    </Form>
   );
 }
