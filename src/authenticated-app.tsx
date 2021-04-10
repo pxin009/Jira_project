c48267074880ae475c4801fc88358c0bdc536985
pxin <1143458272@qq.com>
2021-04-10 17:18:05 +0800

    style: 用emotion的Row组件布局列表页面

--- src/authenticated-app.tsx
+++ src/authenticated-app.tsx
@@ -1,56 +1,43 @@
 import styled from "@emotion/styled"
+import { Row } from "./components/lib"
 import { useAuth } from "./context/auth-context"
 import { ProjectListScreen } from "./screens/project-list"
 
 export const AuthenticatedApp = () => {
   const {logout} = useAuth()
   return <Container>
-    <Header>
-      <HeaderLeft>
-        <h3>logo</h3>
-        <h3>项目</h3>
-        <h3>用户</h3>
-      </HeaderLeft>
+    <Header between={true}>
+      <HeaderLeft gap={true}>
+        <h2>logo</h2>
+        <h2>项目</h2>
+        <h2>用户</h2>
+      </HeaderLeft >
       <HeaderRight>
         <button onClick={logout}>登出</button>
       </HeaderRight>
     </Header>
-    <Nav>nav</Nav>
     <Main>
       <ProjectListScreen/>
     </Main>
-    <Aside>aside</Aside>
-    <Footer>footer</Footer>
   </Container>
 }
 const Container = styled.div`
   display: grid;
-  grid-template-rows: 6rem 1fr 6rem;
-  grid-template-columns: 20rem 1fr 20rem;
-  grid-template-areas: 
-  "header header header"
-  "nav main aside"
-  "footer footer footer";
+  grid-template-rows: 6rem 1fr 6rem;  
   height:100vh;
 `;
-const Header = styled.header`
-  background-color:gray;
+const Header = styled(Row)`
+  /* background-color:gray; */
   /* height:6rem; */
-  grid-area: header;
-  display:flex;
+  /* grid-area: header; */
+  /* display:flex;
   flex-direction:row;
-  align-items:center;
-  justify-content:space-between;
+  align-items:center; */
+  /* justify-content:space-between; */
 `
-const HeaderLeft = styled.div`display:flex;align-items:center;`
+const HeaderLeft = styled(Row)``
 const HeaderRight = styled.div``
 const Main = styled.main`
   /* height:calc(100vh - 6rem); */
-  grid-area: main
+  /* grid-area: main */
 `
-const Nav = styled.nav`grid-area:nav;background-color:pink;
-`
-const Aside = styled.aside`grid-area:aside;background-color:green;
-
-`
-const Footer = styled.footer`grid-area:footer;background-color:blueviolet`
